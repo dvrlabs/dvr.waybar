58122365d4cb5b5945879c47a75496b73501e095
dvr <dvrlabs.tv@gmail.com>
2023-12-07 10:07:43 -0500

    added button to show if muted

---
+++ check_mute.sh
@@ -0,0 +1,11 @@
+MIC_NAME='alsa_input.usb-GeneralPlus_USB_Audio_Device-00.mono-fallback'
+
+# Check mute status
+MUTE_STATUS=$(pactl list sources | awk -v name="$MIC_NAME" '/Name:/{flag=0} $0 ~ name {flag=1} flag && /Mute:/ {print $2; exit}')
+
+if [ "$MUTE_STATUS" = 'yes' ]; then
+    echo "🔇"
+else
+    echo "🔊"
+fi
+
